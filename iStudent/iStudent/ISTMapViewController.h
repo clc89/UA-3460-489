6e1c6f63f14be323ade8e69202c79056f939dc20
Christopher Stoll <stollcri@gmail.com>
2013-11-18 11:50:22 -0500

    Map Views
    Added map view to existing iPad detail view and added a separate map
    view to iPhone story board so that course locations can be displayed.

---
+++ iStudent/iStudent/ISTMapViewController.h
@@ -0,0 +1,14 @@
+//
+//  ISTMapViewController.h
+//  iStudent
+//
+//  Created by Christopher Stoll on 11/18/13.
+//  Copyright (c) 2013 University of Akron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface ISTMapViewController : UIViewController
+
+@end
