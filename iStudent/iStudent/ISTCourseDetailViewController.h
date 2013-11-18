2a4d97886bb37cb0fa5d46fd1444a7800a0d46d5
Christopher Stoll <stollcri@gmail.com>
2013-11-18 14:05:43 -0500

    Course details interface
    Expanded course detail view
    Added taken/required/other icon to course master view

--- iStudent/iStudent/ISTCourseDetailViewController.h
+++ iStudent/iStudent/ISTCourseDetailViewController.h
@@ -14,9 +14,17 @@
 @property (weak, nonatomic) IBOutlet UISwitch *courseTaken;
 @property (weak, nonatomic) IBOutlet UILabel *courseNumber;
 @property (weak, nonatomic) IBOutlet UILabel *courseName;
+@property (weak, nonatomic) IBOutlet UILabel *courseArea;
+@property (weak, nonatomic) IBOutlet UILabel *courseRequired;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *courseTerm;
 @property (weak, nonatomic) IBOutlet UILabel *courseInstructor;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *courseDays;
 @property (weak, nonatomic) IBOutlet UILabel *courseTimeBegin;
 @property (weak, nonatomic) IBOutlet UILabel *courseTimeEnd;
+@property (weak, nonatomic) IBOutlet UILabel *courseLocation;
+
+- (IBAction)didChangeTaken:(id)sender;
+- (IBAction)didChangeTerm:(id)sender;
 
 @end
