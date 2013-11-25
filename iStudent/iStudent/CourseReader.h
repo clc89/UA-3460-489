f0a3661ea899e4a7e35b9e8efa6a591c03923f4d
Corey Conley <coreyconley@Coreys-MacBook-Pro.local>
2013-11-24 22:12:29 -0500

    created CourseReader class to read courses from pList.

---
+++ iStudent/iStudent/CourseReader.h
@@ -0,0 +1,13 @@
+//
+//  CourseReader.h
+//  iStudent
+//
+//  Created by Corey Conley on 11/24/13.
+//  Copyright (c) 2013 University of Akron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CourseReader : NSObject
+
+@end
---
+++ iStudent/iStudent/CourseReader.m
@@ -0,0 +1,13 @@
+//
+//  CourseReader.m
+//  iStudent
+//
+//  Created by Corey Conley on 11/24/13.
+//  Copyright (c) 2013 University of Akron. All rights reserved.
+//
+
+#import "CourseReader.h"
+
+@implementation CourseReader
+
+@end
