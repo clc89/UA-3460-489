ae3b0f00a295b57e2704af94491cfdb32e0f3719
Corey Conley <coreyconley@Coreys-MacBook-Pro.local>
2013-11-24 22:23:18 -0500

    implemented method to read plist and return dictionary.

--- iStudent/iStudent/CourseReader.m
+++ iStudent/iStudent/CourseReader.m
@@ -8,6 +8,30 @@
 
 #import "CourseReader.h"
 
+
+@interface CourseReader (){
+    NSString* documentPlistPath;
+}
+-(NSDictionary *)initializeDataFrompList;
+@end
+
 @implementation CourseReader
+
+
+-(NSDictionary *)initializeDataFrompList{
+    
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* documentsPath = [paths objectAtIndex:0];
+    self->documentPlistPath = [documentsPath stringByAppendingString:@"Data"];
+    NSString* plistPath = self->documentPlistPath;
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:plistPath]) {
+        plistPath = [[NSBundle mainBundle] pathForResource:@"Data" ofType:@"plist"];
+    }
+    
+    NSDictionary *courseData = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+    
+    return courseData;
+}
 
 @end
