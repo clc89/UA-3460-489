78a4a75c8f78e951d05281214d09bd44f43aa6b9
Christopher Stoll <stollcri@gmail.com>
2013-12-06 10:18:53 -0500

    Show table first time
    Show data in the master view when data is first laoded [by allowing the
    table to update when the data changes]

--- iStudent/iStudent/ISTCourseMasterViewController.m
+++ iStudent/iStudent/ISTCourseMasterViewController.m
@@ -15,6 +15,9 @@
 
 @implementation ISTCourseMasterViewController
 
+// TODO: Switch to using the managed object subclass
+// (instead of valueForKey)
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -112,6 +115,56 @@
     return _fetchedResultsController;
 }
 
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.tableView beginUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type
+{
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.tableView endUpdates];
+}
+
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
     NSManagedObject *object = [self.fetchedResultsController objectAtIndexPath:indexPath];
@@ -120,17 +173,18 @@
     
     cell.textLabel.text = [[object valueForKey:@"name"] description];
     cell.detailTextLabel.text = thisCourseNumber;
+    if ([[[object valueForKey:@"name"] description]  isEqual: @"1"]) {
+        cell.imageView.image = [UIImage imageNamed:@"check"];
+    }
 }
-
 
 #pragma mark - Observers
 
 - (void)courseContextProvided:(NSNotification *) notification {
-    NSLog(@"%@", [notification object]);
     self.managedObjectContext = [notification object];
     
+    // No data? Grab some from the seed plist
     if ([self.tableView numberOfRowsInSection:0] <= 0) {
-        // You must allocate memory and initialize these first -- Stoll
         CourseReader *reader = [[CourseReader alloc] init];
         NSDictionary *courseList = [[NSDictionary alloc] init];
         courseList = [reader initializeDataFrompList];
@@ -138,7 +192,6 @@
         
         NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
         NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-        NSLog(@"%@", context);
         for (NSDictionary *course in courses) {
             NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
             
