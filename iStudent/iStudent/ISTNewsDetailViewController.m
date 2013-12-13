ef12082f01271d43c9c63671fa247d1f8d0a3e5b
Scott <pjs35@zips.uakron.edu>
2013-12-13 11:45:50 -0500

    Attempts at directly parsing the output of the UIWebview have proved fruitless.  Not sure of the correct way to do this as a UIWebview isn’t editable, and only accepts an html source to populate it.  Committing my efforts in case someone would build on them.

--- iStudent/iStudent/ISTNewsDetailViewController.m
+++ iStudent/iStudent/ISTNewsDetailViewController.m
@@ -33,6 +33,31 @@
     self.navigationItem.title = self.NewsItem.Title;
     self.DateLabel.text = self.NewsItem.PublicationDate;
     [self.ContentViewer loadHTMLString:self.NewsItem.Content baseURL:NULL];
+    
+    //get text from the webview as javascript
+    NSString *parseString = [self.ContentViewer stringByEvaluatingJavaScriptFromString:
+                            @"document.body.innerText"];
+
+    //split into array and iterate through removing exraneous artifacts from the html
+    NSArray *parsingArray = [parseString componentsSeparatedByString:@" "];
+    NSMutableArray *mutParsingArray = [[NSMutableArray alloc] initWithArray:parsingArray];
+    
+    for (id object in mutParsingArray) {
+        if ([object isEqualToString:@"p"] || [object isEqualToString:@"div"] || [object isEqualToString:@"content"])
+            [mutParsingArray removeObject:object];
+    }
+    
+    
+    //rejoin the array into a string
+    
+    NSString *parsedAccumulator = [[mutParsingArray copy] componentsJoinedByString:@" "];
+    
+    //pass new parsed string to the feed
+    parseString = parsedAccumulator;
+    
+    // UIWebview does not take NSString as a loading option...
+    //[self.ContentViewer parsedAccumulator];
+    
 }
 
 - (void)didReceiveMemoryWarning
